06d5aaf179c3aa7f40b15e82b8711efb680add6a
Pitris Charalambos <charalampospitris1983@gmail.com>
2025-04-10 23:15:57 +0300

    οκ

--- homepage/views.py
+++ homepage/views.py
@@ -85,7 +85,7 @@ def book_content(request,ref_code_book):
     mydata=Books_Images.objects.filter(ref_code=myurl,ref_code_book=ref_code_url).values()
     book_title = mydata.values_list('book_title', flat=True).first()#
     request.session['book_title'] = book_title
-    list_capture = Videos.objects.filter(ref_code=myurl,ref_code_video=ref_code_book).order_by('chapter_title', 'id').distinct('chapter_title').values_list('chapter_title', flat=True)#διαγράφω τα dublicates
+    list_capture = Videos.objects.filter(ref_code=myurl,ref_code_video=ref_code_book).order_by('chapter_title', 'id').values_list('chapter_title', flat=True).distinct('chapter_title')#διαγράφω τα dublicates
     template = loader.get_template('book_content.html')
     context = {
         'data': mydata,
